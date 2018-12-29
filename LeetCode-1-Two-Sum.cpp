0d0f4c7f808e66eb5267e4f536dd584efdd58cad
LeetKing1 <46234413+LeetKing1@users.noreply.github.com>
2018-12-28 19:20:39 -0800

    Create LeetCode-1-Two-Sum.cpp

---
+++ LeetCode-1-Two-Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap; 
+        int index, key; 
+        
+        for( index = 0; index < nums.size( ); index++ )
+        {
+            key = target - nums[ index ];
+            
+            if( hashMap.count( key ) != 0 )
+            {
+                return { hashMap[ key ], index };
+            }
+            
+            hashMap.insert( { nums[index], index } );
+        }
+        
+        return { -1, -1 };
+    }
+};
